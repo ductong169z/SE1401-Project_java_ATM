48726996da1a7c7f9ecf8f5153cf9b77a42371c1
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2019-12-03 18:19:04 +0700

    [Update class Menu] Update method createAccount() and changeDepositLimit()
    - Add queries to add user info to database
    - Add menu to changeDepositLimit method

--- src/atm/ATM.java
+++ src/atm/ATM.java
@@ -64,18 +64,22 @@ class Auth {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter CARD ID:");
         int card_id = input.nextInt();
+        input.nextLine();
+        
         System.out.println("Please enter PIN:");
         int pin = 0;
         boolean c = true;
         do {
             try {
                 pin = input.nextInt();
+                input.nextLine();
                 c = false;
             } catch (InputMismatchException e) {
 
                 System.out.println("Only number here !!");
             }
         } while (c);
+        
         if (card_id == rs.getInt(2) && pin == rs.getInt(3)) {
 
             UserInfo user = new UserInfo(rs.getInt(1), rs.getString(7), rs.getInt(8));
@@ -98,44 +102,113 @@ class Menu {
     // random from 10000000 to 10099999
     public int randomCardID() {
         Random rand = new Random(System.currentTimeMillis());
+<<<<<<< Updated upstream
         int randNum = rand.nextInt(99999) + 10000000;
         return randNum;
+=======
+        return rand.nextInt(100000) + 10000000;
+>>>>>>> Stashed changes
     }
 
     // random from 1000 to 9999
     public int randomPIN() {
         Random rand = new Random(System.currentTimeMillis());
-        int randNum = rand.nextInt(9000) + 1000;
-        return randNum;
+        return rand.nextInt(9000) + 1000;
     }
 
     public void createAccount() throws SQLException, ClassNotFoundException {
         // random card ID and PIN, check to make sure card ID is unique
         boolean check = true;
 
+        int cardID;
         Connection conn = DataConnection.getConnection();
         Statement stmt = conn.createStatement();
         do {
-            int cardID = randomCardID();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE card_id = " + cardID);
+            cardID = randomCardID();
+            ResultSet getCardID = stmt.executeQuery("SELECT * FROM users WHERE card_id = " + cardID);
             // if there is already such card ID in database, random again
-            if (rs.next()) {
+            if (getCardID.next()) {
                 check = false;
             }
         } while (!check);
-
+        
         int PIN = randomPIN();
-
+        
         // filling user account's basic details
-        String name = "";
-        String contactNumber = "";
-        String gender = "";
-        String address = "";
+        String name = "Name";
+        String contactNumber = "contact number";
+        int gender = 1;
+        String address = "ngoc yen";
+
+        stmt.executeUpdate("INSERT INTO users(card_id, pin, contact_number, gender, address, name) VALUES(" + cardID + "," + PIN + ", \"" + contactNumber + "\" ," + gender + ", \"" + address + "\" , \"" + name + "\")");
+        ResultSet insertInfo = stmt.executeQuery("SELECT id FROM users WHERE card_id = " + cardID);
+        insertInfo.next();
+        int userID = insertInfo.getInt(1);
+        stmt.executeUpdate("INSERT INTO user_role(user_id, role_id) VALUES(" + userID + "," + 2 + ")");
+        stmt.executeUpdate("INSERT INTO user_money(user_id, total_money) VALUES(" + userID + "," + 0 + ")");
 
     }
 
     public void changeDepositLimit() {
+        int choice = 0;
+        boolean check = true; // validate if input is valid (by default input is valid)
+        int depositLimit; // store deposit limit
+        int depositNumLimit; // store the limit of number of deposits
+        
+        do {
+            System.out.println("What would you like to do?");
+            System.out.println("1. Change deposit limit");
+            System.out.println("2. Change amount of deposits limit");
+            System.out.println("3. Revert deposit limit to default value (25000$)");
+            System.out.println("4. Revert amount of deposits limit to default value (5 times)");
+            System.out.println("5. Exit");
+            do {
+                try {
+                    Scanner input = new Scanner(System.in);
 
+                    System.out.print("Enter your choice: ");
+                    choice = input.nextInt();
+                    input.nextLine();
+
+                    if (choice < 1 || choice > 7) {
+                        check = false;
+                        System.out.println("Please enter a number from 1 to 5 ");
+                        System.out.println("");
+                    }
+                } catch (InputMismatchException ex) {
+                    check = false;
+                    System.out.println("Please enter a number ");
+                    System.out.println("");
+                } catch (Exception ex) {
+                    check = false;
+                    System.out.println("An error occured! Please try again later!");
+                    System.out.println("");
+                }
+            } while (!check);
+            
+            switch (choice){
+                case 1:
+                    
+                    
+                    break;
+                    
+                case 2:
+                    
+                    break;
+                    
+                case 3:
+                    
+                    break;
+                    
+                case 4:
+                    
+                    break;
+                    
+                case 5:
+                    
+                   
+            }
+        } while (choice != 5);
     }
 
     public void changeWithdrawalLimit() {
@@ -198,23 +271,29 @@ class Menu {
                     break;
 
                 case 2:
-
+                    changeDepositLimit();
+                    
+                    
                     break;
 
                 case 3:
-
+                    changeWithdrawalLimit();
+                    
                     break;
 
                 case 4:
-
+                    changeTransCount();
+                    
                     break;
 
                 case 5:
-
+                    createDepositReport();
+                    
                     break;
 
                 case 6:
-
+                    createWithdrawalReport();
+                            
                     break;
 
                 case 7:
@@ -255,6 +334,9 @@ public class ATM {
             System.out.println("Input numbers only!");
         }
 
+        Menu obj = new Menu();
+        
+        obj.createAccount();
     }
-
+    
 }
