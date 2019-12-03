4eeee88c4e42d7e59de85f746c64c5d51c44b4b6
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2019-12-03 16:07:09 +0700

    Update ATM.java

--- src/atm/ATM.java
+++ src/atm/ATM.java
@@ -93,51 +93,6 @@ class Menu {
     }
 }
 
-class Menu {
-
-    public Menu() {
-    }
-
-    public int random() {
-        Random rand = new Random(System.currentTimeMillis());
-        int randNum = rand.nextInt(9000) + 1000;
-        return randNum;
-    }
-
-    public void displayAdminMenu() {
-        int choice, check;
-
-        System.out.println("------------ ADMINISTRATION ------------");
-        System.out.println("1. Creating new user account");
-        System.out.println("2. Change deposit limitation");
-        System.out.println("3. Change withdrawal limitation");
-        System.out.println("4. Change the number of last transactions on display");
-        System.out.println("5. Create deposit report");
-        System.out.println("6. Create withdrawal report");
-        do {
-            try {
-                Scanner input = new Scanner(System.in);
-                // by default input for "choice" is valid
-                check = 1;
-
-                System.out.print("Enter your choice: ");
-                choice = input.nextInt();
-                input.nextLine();
-
-                if (choice < 1 || choice > 6) {
-                    check = 0;
-                    System.out.println("Please enter a number from 1 to 6 ");
-                    System.out.println("");
-                }
-            } catch (InputMismatchException ex) {
-                check = 0;
-                System.out.println("Please enter a number ");
-                System.out.println("");
-            }
-        } while (check == 0);
-    }
-}
-
 public class ATM {
 
     /**
